aa26adf33c03ccb7512a8bf2ec212fda4af0d01c
zwithe <zwithe133@gmail.com>
2024-05-02 12:38:38 -0400

    inital commit started skeleton

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Final-project
+Photo by <a href="https://unsplash.com/@kellysikkema?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Kelly Sikkema</a> on <a href="https://unsplash.com/photos/white-spiral-notebook-on-brown-wooden-table-2q_frVRXWfQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+Photo by <a href="https://unsplash.com/@enginakyurt?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">engin akyurt</a> on <a href="https://unsplash.com/photos/white-book-with-blue-pen-IZj7vckPGiw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+ 
+  
---
+++ index.js
@@ -0,0 +1,23 @@
+//dependencies
+const express = require("express")
+const methodOverride = require('method-override')
+const { Sequelize } = require('sequelize')
+require('dotenv').config()
+
+// CONFIGURATION / MIDDLEWARE
+const app = express()
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: true }))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
